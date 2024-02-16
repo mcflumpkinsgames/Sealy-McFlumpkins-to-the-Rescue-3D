94173251127565d62577eb10e445de3dbbaa2bd0
mcflumpkinsgames <margaret.k.mcnulty+gamesgit@gmail.com>
2024-02-16 15:11:50 -0800

    improved collision handling and sounds for collision, still needs some work though

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     int currentSceneIndex;
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] AudioClip victory;
+    [SerializeField] AudioClip defeat;
+
+    AudioSource audioSource;
 
     void Start()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnCollisionEnter(Collision other) {
         switch (other.gameObject.tag)
@@ -19,22 +25,42 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Fibsh!!");
                 break;
             case "Finish":
-                int nextScene = currentSceneIndex+1;
-                if (nextScene < SceneManager.sceneCountInBuildSettings) {
-                    SceneManager.LoadScene(nextScene);
-                } else {
-                    Debug.Log("look at me look at me I'm a winner");
-                }
+                StartVictorySequence();
                 break;
             default:
-                Debug.Log("Owwww wtf");
-                ReloadScene();
+                StartCrashSequence();
                 break;
         }
     }
 
     void ReloadScene() {
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextScene()
+    {
+        int nextScene = currentSceneIndex+1;
+        if (nextScene < SceneManager.sceneCountInBuildSettings) {
+            SceneManager.LoadScene(nextScene);
+        } else {
+            Debug.Log("look at me look at me I'm a winner");
+        }
+    }
+
+    void StartCrashSequence()
+    {
+        audioSource.PlayOneShot(defeat);
+        GetComponent<Movement>().enabled = false;
+        Debug.Log("Womp womp, you died");
+        Invoke("ReloadScene", loadDelay);
+    }
+
+    void StartVictorySequence()
+    {
+        audioSource.PlayOneShot(victory);
+        GetComponent<Movement>().enabled = false;
+        Debug.Log("You can't fight the power of the seal team");
+        Invoke("LoadNextScene", loadDelay);
     }
 
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Credits
+Womp, Womp Defeat Sound: https://freesound.org/people/TravieDoodle/
+Victory fanfare: https://freesound.org/people/Sheyvan/
